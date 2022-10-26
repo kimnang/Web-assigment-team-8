4dfe5f9dace346542b88913d58f7005a050853b2
kimnang <49425329+kimnang@users.noreply.github.com>
2022-10-26 22:34:07 +0700

    update readmore

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,4 @@
 $(window).on('load', function(){
 	// Preloader JS
-	$(".preloader_wrapper").fadeOut();
-	$(".preloader_inner").delay(1000).fadeOut("slow");
+	$(".preloader").delay(1000).fadeOut();
 });
